61806dd9b3ae215ffc2805b8b82f58137b8ee649
Brownbatz <Lopeztgabriel4@gmail.com>
2020-03-23 20:19:56 -0600

    ALmost at mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      players: [{}],
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({...this.state, players: result})
+        }
+      )
+    console.log("Here are the players in state: ", this.state.players); // this doesn't mattermn 
+  };
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is just a placeholder</h1>
+      <div>
+        {this.state.players.map(i => {
+          return(
+          <h1>{i.name}</h1>
+          )
+        })}
+      </div>
     </div>
-  );
+  );}
 }
 
 export default App;
---
+++ client/src/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// I can make a custom hook here for this function but for right now I will use the set state method as that would be easiest
+
+export default function Player({props}){
+
+    return(
+        <div className="player">
+            
+        </div>
+    )
+}
